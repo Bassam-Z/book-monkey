4d4389d1b48b80c2a8ef0c88d9a2aa18a244ca9e
Bassam.Z <bassamz.ali@gmail.com>
2021-09-10 12:28:51 +0200

    Bis Kapitel 8.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { types } from 'util';
+
+import { Book } from './shared/book';
+
+type ViewState ='list' | 'details';
 
 @Component({
   selector: 'bm-root',
@@ -6,5 +11,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'book-monkey';
+  book: Book;
+  viewState: ViewState='list';
+
+  showList(){
+    this.viewState = 'list';
+  }
+  showDetails(book: Book){
+    this.book = book;
+    this.viewState ='details';
+
+  }
+  // title = 'book-monkey';
 }
